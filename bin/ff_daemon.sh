3908af924e3f1a0c45451a2b490f93f819441e95
NAKAMURA <nakahiro@gmail.com>
2009-04-08 12:59:21 +0900

    added bin/ff_daemon.sh for a daemon.  [#169]
    This daemon must be invoked independently from Rails app.  Edit the path
    to ruby at the first line of bin/ff_daemon.sh and run it like;
    % bin/ff_daemon.sh
    It spawn a daemon process for accessing FriendFeed API server instead of
    Rail app; Rails app connects to the daemon via druby.
    Send SIGINT to the process to stop the daemon.  PID is dumped at
    bin/ff_daemon.pid.

---
+++ bin/ff_daemon.sh
@@ -0,0 +1,45 @@
+#!/usr/local/bin/ruby
+
+ENV['RAILS_ENV'] ||= 'production'
+
+dir = File.dirname(__FILE__)
+libdir = File.expand_path('../lib', dir)
+pid_file = File.expand_path('ff_daemon.pid', dir)
+
+$: << libdir
+
+require File.expand_path('../config/environment', dir)
+require 'ff_daemon'
+require 'webrick'
+require 'logger'
+
+if File.exist?(pid_file)
+  pid = File.open(pid_file) { |f| f.gets }.to_i
+  begin
+    Process.kill(:INT, pid)
+  rescue SystemCallError
+  end
+end
+
+Process.daemon unless $DEBUG
+
+File.open(pid_file, 'w') do |f|
+  f.puts($$)
+end
+
+front = FriendFeed::APIDaemon.new
+front.client.logger = RAILS_DEFAULT_LOGGER
+front.client.apikey = F2P::Config.friendfeed_api_key
+front.client.http_proxy = F2P::Config.http_proxy
+DRb.start_service(F2P::Config.friendfeed_api_daemon_drb_uri, front)
+
+trap(:INT) do
+  DRb.stop_service
+  exit!
+end
+
+if $DEBUG
+  gets
+else
+  DRb.thread.join
+end
