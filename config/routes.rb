6a02d4c3c835086c3c9776eacfc721fcf1f22385
NAKAMURA <nakahiro@gmail.com>
2009-04-25 22:18:43 +0900

    create index page.  closes #44.

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => 'entry'
+  # map.root :controller => 'entry'
 
   # See how all your routes lay out with "rake routes"
 
