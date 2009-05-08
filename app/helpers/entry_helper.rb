65866426764f03286442546c0479152ac9e7e136
NAKAMURA <nakahiro@gmail.com>
2009-05-09 00:09:21 +0900

    show pin_link always in [pin] view. closes #78.

--- app/helpers/entry_helper.rb
+++ app/helpers/entry_helper.rb
@@ -81,7 +81,7 @@ module EntryHelper
   end
 
   def pin_link(entry)
-    if ctx.inbox or ctx.home or ctx.single? or ctx.query or entry.view_pinned
+    if ctx.inbox or ctx.home or ctx.single? or ctx.label or ctx.query or entry.view_pinned
       if entry.view_pinned
         link_to(icon_tag(:pinned, 'unpin'), link_action('unpin', :id => entry.id))
       else
