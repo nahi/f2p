548e789b629b74cdd0965010072c957ca59b9438
NAKAMURA <nakahiro@gmail.com>
2009-05-28 07:03:58 +0900

    show login message.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -29,6 +29,7 @@ class LoginController < ApplicationController
       if user = User.validate(name, remote_key)
         set_user(user)
         set_timezone(tz)
+        flash[:message] = "You can change font size and other settings. Follow the 3rd 'gear' icon above."
         if params = session[:redirect_to_after_authenticate]
           session[:redirect_to_after_authenticate] = nil
           redirect_to url_for(params)
