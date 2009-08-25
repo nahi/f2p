b80872cbf5cc4ca2c6ec324a4d1eb28f197aa829
NAKAMURA <nakahiro@gmail.com>
2009-08-25 22:12:24 +0900

    cosmetic

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -7,7 +7,7 @@ Click the banner for granting access to FriendFeed by f2p.
 You don't have to register your credential to f2p site.
 (You're redirected to FriendFeed Web site. It's easy to follow 1 or 2 steps.)<br />
 #{oauth_image_tag}<br />
-(Unfortunately it doesn't work for Japanese cell phones.)
+(Unfortunately it doesn't work for some cell phones. Please use remote key sign in below when it doesn't work.)
 </p>
 <p>
 You can remove the grant anytime from #{link_to('http://friendfeed.com/settings/applications', 'http://friendfeed.com/settings/applications')}.
