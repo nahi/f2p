d5db49fd6aa4fb485188e52e01603d79715e6587
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-03-26 01:17:34 +0900

    Remove unused ff_daemon lib.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
-require 'ff_daemon'
+require 'ff'
 require 'stringio'
 require 'zlib'
 require 'ext'
