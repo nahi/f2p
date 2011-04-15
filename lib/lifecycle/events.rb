e618369140d98e3c72dfd9d6f9896e2e5e773eda
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-04-15 16:08:58 +0900

    Fix the bug overwriting logfile everytime.
    Stupid.

--- lib/lifecycle/events.rb
+++ lib/lifecycle/events.rb
@@ -7,7 +7,7 @@ module Trinidad::Lifecycle::WebApp
     include Trinidad::Tomcat::LifecycleListener
 
     def lifecycle_event(event)
-      File.open("/tmp/lifecycle_event.log", "wb") do |file|
+      File.open("/tmp/lifecycle_event.log", "a") do |file|
         logger = Logger.new(file)
         logger.info "*** INFO: Firing event: #{event.type.to_s} - #{event}"
         logger.info "data: #{event.data}"
